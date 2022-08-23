817fe4cd05d14820e804cf69e4371940ce8f8ab7
Haashir-Shakeel <david10james99@gmail.com>
2022-08-23 16:34:45 +0500

    getting data from backend

--- src/pages/NotesListPage.js
+++ src/pages/NotesListPage.js
@@ -10,12 +10,19 @@ useEffect(()=>{
 const getNotes = async () => {
   const response = await fetch('http://127.0.0.1:8000/api/notes/')
   const data = await response.json()
-  console.log('DATA:',data)
   setNotes(data)
 }
 
   return (
-    <div>Notes</div>
+    <div>
+      <div className='notes-list'>
+        {
+        notes.map((note, index)=> {
+          return <h4 key={index}>{note.body}</h4>
+        })
+        }
+      </div>
+    </div>
   )
 }
 
