cab2a5d7ad14122fdc8860e3b7ba4d9d94eab341
Haashir-Shakeel <david10james99@gmail.com>
2022-08-29 17:47:32 +0500

    getting updatedDate and body in Note List Display

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -9,11 +9,31 @@ const getTitle = (note) => {
   return title
 }
 
+const getTime = (note) => {
+  return new Date(note.updated).toLocaleDateString()
+}
+
+const getBody = (note) => {
+    const title = getTitle(note)
+    let content = note.body.replaceAll('\n',' ') // replace all new line with space(instead of having new line we will add space )
+    content = content.replaceAll(title,'') //replacing title with empty string and having just body
+
+    if (content.length > 45){
+      return content.slice(0, 45) + '...'
+    }else{
+      return content
+    }
+  }
+
 export const ListItem = (props) => {
   return (
     <Link to={`/note/${props.note.id}`}>
       <div className='notes-list-item'>
         <h3>{getTitle(props.note)}</h3>
+        <p>
+          <span>{getTime(props.note)}</span>
+          {getBody(props.note)}
+        </p>
       </div>
       </Link>
       
